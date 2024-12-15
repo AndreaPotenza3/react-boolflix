8b0cf4c7f5d1d527f127cc6e39df78dc9915b658
AndreaPotenza <andreapoten3@gmail.com>
2024-12-16 00:16:20 +0100

    corretto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import globalContext from './context/context'
+import axios from 'axios'
 import Main from './components/main/Main'
 import Header from './components/header/Header'
 
 function App() {
-
   const api_key = '2975762f0c91d8d64c2e7882335dc0ea'
 
   const [filmList, setFilmList] = useState([])
   const [seriesList, setSeriesList] = useState([])
   const [query, setQuery] = useState('')
+
 
   function fetchData() {
     axios.get('https://api.themoviedb.org/3/search/movie', {
@@ -20,7 +21,7 @@ function App() {
     })
       .then(res => {
         console.log(res.data)
-        setFilmList(res.data, result)
+        setFilmList(res.data.results)
       }).catch(err => {
         console.error(err)
         setFilmList([])
@@ -34,18 +35,20 @@ function App() {
     })
       .then(res => {
         console.log(res.data)
-        setSeriesList(res.data, result)
+        setSeriesList(res.data.results)
       }).catch(err => {
         console.error(err)
         setSeriesList([])
       })
   }
 
-  <globalContext.Provider value={filmList, seriesList, query, setQuery, fetchData}>
-    <Header />
-    <Main />
-  </globalContext.Provider>
+  return (
+    <globalContext.Provider value={{ filmList, seriesList, query, setQuery, fetchData }}>
+      <Header />
+      <Main />
+    </globalContext.Provider>
 
+  )
 }
 
 export default App
