396ee22c64dd17600c055089dc0baae85052a48d
AndreaPotenza <andreapoten3@gmail.com>
2024-12-16 08:53:02 +0100

    aggiunte bandiere

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,16 @@
+import itFlag from '../../assets/itFlag.webp'
+import enFlag from '../../assets/enFlag.avif'
+import frFlag from '../../assets/frFlag.jpg'
+import deFlag from '../../assets/deFlag.webp'
+
 export default function Card({ item }) {
+
+    const flags = {
+        it: itFlag,
+        en: enFlag,
+        fr: frFlag,
+        de: deFlag,
+    }
 
     const { title, name, original_title, original_name, vote_average, original_language, poster_path } = item
 
@@ -6,7 +18,7 @@ export default function Card({ item }) {
         <div>
             <h3>{title || name}</h3>
             <p>{original_title || original_name}</p>
-            <p>{original_language}</p>
+            <img src={flags[original_language]} height='20' alt="" />
             <p>{vote_average}</p>
         </div>
     )
